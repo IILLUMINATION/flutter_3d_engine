6c5fc4cab460478b16f3c462cc96e2ffd35cb7ad
IILLUMINATION <IILLUMINAT@outlook.com>
2026-07-14 20:32:24 +0300

    Fix wgpu usage conflict: separate encoders for opaque + composite passes
    - Two CommandEncoders: opaque renders to opaque_texture, composite copies
      opaque->render_texture via copy_texture_to_texture, then draws water on top
    - Added present_from_dual() to FrameSink trait
    - Proper usage flags: opaque_texture gets COPY_SRC+TEXTURE_BINDING+RENDER_ATTACHMENT,
      render_texture gets COPY_SRC+COPY_DST+RENDER_ATTACHMENT

--- rust/src/core/renderer_gpu.rs
+++ rust/src/core/renderer_gpu.rs
@@ -135,6 +135,8 @@ pub struct GpuRenderer<S: FrameSink = crate::core::present::CpuBufferSink> {
     render_texture_view: wgpu::TextureView,
     depth_texture: wgpu::Texture,
     depth_view: wgpu::TextureView,
+    opaque_texture: wgpu::Texture,
+    opaque_texture_view: wgpu::TextureView,
     water_pipeline: wgpu::RenderPipeline,
     water_bind_group_layout: wgpu::BindGroupLayout,
     water_normal_texture: wgpu::Texture,
@@ -386,7 +388,7 @@ impl<S: FrameSink> GpuRenderer<S> {
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
             format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::COPY_SRC | wgpu::TextureUsages::TEXTURE_BINDING,
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::COPY_SRC | wgpu::TextureUsages::COPY_DST,
             view_formats: &[],
         });
         let render_texture_view = render_texture.create_view(&Default::default());
@@ -402,6 +404,18 @@ impl<S: FrameSink> GpuRenderer<S> {
             view_formats: &[],
         });
         let depth_view = depth_texture.create_view(&Default::default());
+
+        let opaque_texture = device.create_texture(&wgpu::TextureDescriptor {
+            label: Some("opaque texture"),
+            size: wgpu::Extent3d { width, height, depth_or_array_layers: 1 },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_SRC,
+            view_formats: &[],
+        });
+        let opaque_texture_view = opaque_texture.create_view(&Default::default());
 
         let screen_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
             label: Some("screen sampler"),
@@ -599,7 +613,7 @@ impl<S: FrameSink> GpuRenderer<S> {
                 },
                 wgpu::BindGroupEntry {
                     binding: 4,
-                    resource: wgpu::BindingResource::TextureView(&render_texture_view),
+                    resource: wgpu::BindingResource::TextureView(&opaque_texture_view),
                 },
             ],
         });
@@ -624,6 +638,8 @@ impl<S: FrameSink> GpuRenderer<S> {
             render_texture_view,
             depth_texture,
             depth_view,
+            opaque_texture,
+            opaque_texture_view,
             water_pipeline,
             water_bind_group_layout: water_bg_layout,
             water_normal_texture,
@@ -648,7 +664,7 @@ impl<S: FrameSink> GpuRenderer<S> {
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
             format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::COPY_SRC | wgpu::TextureUsages::TEXTURE_BINDING,
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::COPY_SRC | wgpu::TextureUsages::COPY_DST,
             view_formats: &[],
         });
         self.render_texture_view = self.render_texture.create_view(&Default::default());
@@ -664,6 +680,18 @@ impl<S: FrameSink> GpuRenderer<S> {
             view_formats: &[],
         });
         self.depth_view = self.depth_texture.create_view(&Default::default());
+
+        self.opaque_texture = self.device.create_texture(&wgpu::TextureDescriptor {
+            label: Some("opaque texture"),
+            size: wgpu::Extent3d { width, height, depth_or_array_layers: 1 },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_SRC,
+            view_formats: &[],
+        });
+        self.opaque_texture_view = self.opaque_texture.create_view(&Default::default());
 
         self.water_bind_group = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
             label: Some("water bind group"),
@@ -687,7 +715,7 @@ impl<S: FrameSink> GpuRenderer<S> {
                 },
                 wgpu::BindGroupEntry {
                     binding: 4,
-                    resource: wgpu::BindingResource::TextureView(&self.render_texture_view),
+                    resource: wgpu::BindingResource::TextureView(&self.opaque_texture_view),
                 },
             ],
         });
@@ -796,47 +824,50 @@ impl<S: FrameSink> GpuRenderer<S> {
         }
 
         let color_attach = wgpu::RenderPassColorAttachment {
-            view: &self.render_texture_view,
+            view: &self.opaque_texture_view,
             depth_slice: None,
             resolve_target: None,
             ops: wgpu::Operations { load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.12, g: 0.12, b: 0.16, a: 1.0 }), store: wgpu::StoreOp::Store },
         };
 
-        let mut encoder = self.device.create_command_encoder(&Default::default());
         {
-            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("opaque pass"),
-                color_attachments: &[Some(color_attach)],
-                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                    view: &self.depth_view,
-                    depth_ops: Some(wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(1.0),
-                        store: wgpu::StoreOp::Store,
+            let mut encoder = self.device.create_command_encoder(&Default::default());
+            {
+                let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: Some("opaque pass"),
+                    color_attachments: &[Some(color_attach)],
+                    depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                        view: &self.depth_view,
+                        depth_ops: Some(wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(1.0),
+                            store: wgpu::StoreOp::Store,
+                        }),
+                        stencil_ops: None,
                     }),
-                    stencil_ops: None,
-                }),
-                timestamp_writes: None,
-                occlusion_query_set: None,
-                multiview_mask: None,
-            });
+                    timestamp_writes: None,
+                    occlusion_query_set: None,
+                    multiview_mask: None,
+                });
 
-            rpass.set_pipeline(&self.grid_pipeline);
-            rpass.set_bind_group(0, &self.camera_bind_group, &[]);
-            rpass.set_vertex_buffer(0, self.grid_buffer.slice(..));
-            rpass.draw(0..grid_vcount, 0..1);
-
-            if gizmo_vert_count > 0 {
-                rpass.set_vertex_buffer(0, self.gizmo_buffer.slice(..));
-                rpass.draw(0..gizmo_vert_count, 0..1);
-            }
-
-            if num_instances > 0 {
-                rpass.set_pipeline(&self.render_pipeline);
+                rpass.set_pipeline(&self.grid_pipeline);
                 rpass.set_bind_group(0, &self.camera_bind_group, &[]);
-                rpass.set_bind_group(1, &self.models_bind_group, &[]);
-                rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-                rpass.draw(0..self.num_vertices, 0..num_instances);
+                rpass.set_vertex_buffer(0, self.grid_buffer.slice(..));
+                rpass.draw(0..grid_vcount, 0..1);
+
+                if gizmo_vert_count > 0 {
+                    rpass.set_vertex_buffer(0, self.gizmo_buffer.slice(..));
+                    rpass.draw(0..gizmo_vert_count, 0..1);
+                }
+
+                if num_instances > 0 {
+                    rpass.set_pipeline(&self.render_pipeline);
+                    rpass.set_bind_group(0, &self.camera_bind_group, &[]);
+                    rpass.set_bind_group(1, &self.models_bind_group, &[]);
+                    rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+                    rpass.draw(0..self.num_vertices, 0..num_instances);
+                }
             }
+            self.queue.submit(Some(encoder.finish()));
         }
 
         {
@@ -849,37 +880,39 @@ impl<S: FrameSink> GpuRenderer<S> {
             };
             self.queue.write_buffer(&self.water_uniform_buffer, 0, bytemuck::bytes_of(&water_uniforms));
 
-            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("composite pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &self.render_texture_view,
-                    depth_slice: None,
-                    resolve_target: None,
-                    ops: wgpu::Operations { load: wgpu::LoadOp::Load, store: wgpu::StoreOp::Store },
-                })],
-                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                    view: &self.depth_view,
-                    depth_ops: Some(wgpu::Operations { load: wgpu::LoadOp::Load, store: wgpu::StoreOp::Store }),
-                    stencil_ops: None,
-                }),
-                timestamp_writes: None,
-                occlusion_query_set: None,
-                multiview_mask: None,
-            });
+            let mut encoder = self.device.create_command_encoder(&Default::default());
+            {
+                let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: Some("composite pass"),
+                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                        view: &self.render_texture_view,
+                        depth_slice: None,
+                        resolve_target: None,
+                        ops: wgpu::Operations { load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }), store: wgpu::StoreOp::Store },
+                    })],
+                    depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                        view: &self.depth_view,
+                        depth_ops: Some(wgpu::Operations { load: wgpu::LoadOp::Load, store: wgpu::StoreOp::Store }),
+                        stencil_ops: None,
+                    }),
+                    timestamp_writes: None,
+                    occlusion_query_set: None,
+                    multiview_mask: None,
+                });
 
-            if self.water_index_count > 0 {
-                rpass.set_pipeline(&self.water_pipeline);
-                rpass.set_bind_group(0, &self.water_bind_group, &[]);
-                rpass.set_vertex_buffer(0, self.water_vertex_buffer.slice(..));
-                rpass.set_index_buffer(self.water_index_buffer.slice(..), wgpu::IndexFormat::Uint32);
-                rpass.draw_indexed(0..self.water_index_count, 0, 0..1);
+                if self.water_index_count > 0 {
+                    rpass.set_pipeline(&self.water_pipeline);
+                    rpass.set_bind_group(0, &self.water_bind_group, &[]);
+                    rpass.set_vertex_buffer(0, self.water_vertex_buffer.slice(..));
+                    rpass.set_index_buffer(self.water_index_buffer.slice(..), wgpu::IndexFormat::Uint32);
+                    rpass.draw_indexed(0..self.water_index_count, 0, 0..1);
+                }
             }
+            self.queue.submit(Some(encoder.finish()));
         }
 
-        self.queue.submit(Some(encoder.finish()));
-
         self.sink
-            .present(&self.device, &self.queue, &self.render_texture)
+            .present_from_dual(&self.device, &self.queue, &self.opaque_texture, &self.render_texture)
     }
 }
 
